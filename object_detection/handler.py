267e2cbf40324eb1dc188ac02271d7a5f97470dd
fgtubs <87946264+fgtubs@users.noreply.github.com>
2023-01-19 14:15:26 +0100

    Delete handler.py

--- object_detection/handler.py
+++
@@ -1,8 +0,0 @@
-from function import preprocess
-
-
-def handle(req):
-    buf = ""
-    for line in str(req):
-        buf = buf + line
-    return preprocess.download_and_resize_image(buf, 1280, 856, False)
