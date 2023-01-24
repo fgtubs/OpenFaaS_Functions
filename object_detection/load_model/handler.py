309316c052f6e2d2a3a3e4712573416aae5f3196
fgtubs <87946264+fgtubs@users.noreply.github.com>
2023-01-24 17:18:19 +0100

    Delete handler.py

--- object_detection/load_model/handler.py
+++
@@ -1,8 +0,0 @@
-from function import load_image
-
-
-def handle(req):
-    buf = ""
-    for line in str(req):
-        buf = buf + line
-    return load_image.download_and_resize_image(buf, 1280, 856, False)
