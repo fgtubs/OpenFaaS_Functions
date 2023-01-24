20d5cc05a09acad390b58baefd5c424388f58d23
fgtubs <87946264+fgtubs@users.noreply.github.com>
2023-01-24 17:19:28 +0100

    Create handler.py

---
+++ object_detection/load_model/function/handler.py
@@ -0,0 +1,8 @@
+from function import laod_model
+
+
+def handle(req):
+    buf = ""
+    for line in str(req):
+        buf = buf + line
+    return load_model.download_and_resize_image_and_load(buf, 1280, 856, False)
