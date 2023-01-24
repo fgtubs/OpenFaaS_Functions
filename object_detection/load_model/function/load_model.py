0e0d17b8cee77189a4f0c6982fa64801c3c3cbb7
fgtubs <87946264+fgtubs@users.noreply.github.com>
2023-01-24 17:18:06 +0100

    Create load_model.py

---
+++ object_detection/load_model/function/load_model.py
@@ -0,0 +1,35 @@
+# For downloading the image.
+from PIL import Image
+from PIL import ImageOps
+import matplotlib.pyplot as plt
+import tempfile
+from six.moves.urllib.request import urlopen
+from six import BytesIO
+import tensorflow_hub as hub
+
+
+
+
+def download_and_resize_image_and_load(url, new_width=256, new_height=256,
+                              display=False):
+  _, filename = tempfile.mkstemp(suffix=".jpg")
+  response = urlopen(url)
+  image_data = response.read()
+  image_data = BytesIO(image_data)
+  pil_image = Image.open(image_data)
+  pil_image = ImageOps.fit(pil_image, (new_width, new_height), Image.ANTIALIAS)
+  pil_image_rgb = pil_image.convert("RGB")
+  pil_image_rgb.save(filename, format="JPEG", quality=90)
+  print("Image downloaded to %s." % filename)
+  if display:
+    display_image(pil_image)
+  print("start loading detector")
+  #MODEL_PATH = "function/model"
+  MODEL_PATH = "/home/vagrant/model"
+  detector = hub.load(MODEL_PATH).signatures['default']
+  print("detector loaded")
+  return filename
+
+if __name__ == "__main__":
+  image_url = "https://upload.wikimedia.org/wikipedia/commons/6/60/Naxos_Taverna.jpg" 
+  downloaded_image_path = download_and_resize_image(image_url, 1280, 856, False)
