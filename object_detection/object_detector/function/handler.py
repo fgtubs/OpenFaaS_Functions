25ef465ecaa8f78e148c781354d59889f11510be
fgtubs <87946264+fgtubs@users.noreply.github.com>
2023-01-27 11:11:48 +0100

    Update handler.py

--- object_detection/object_detector/function/handler.py
+++ object_detection/object_detector/function/handler.py
@@ -1,9 +1,9 @@
-from function import load_model
+from function import detector 
 
 
 def handle(req):
     buf = ""
     for line in str(req):
         buf = buf + line
-    image_path = load_model.download_and_resize_image_and_load(buf, 1280, 856, False)
+    image_path = load_model.download_and_resize_image(buf, 1280, 856, False)
     return detector.run_detector(image_path)
