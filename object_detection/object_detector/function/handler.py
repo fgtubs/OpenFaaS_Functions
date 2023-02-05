a7cd001585ad9a548be4e0e8bca58f91ecbdfc5e
fgtubs <87946264+fgtubs@users.noreply.github.com>
2023-02-05 22:59:41 +0100

    Update handler.py

--- object_detection/object_detector/function/handler.py
+++ object_detection/object_detector/function/handler.py
@@ -5,5 +5,5 @@ def handle(req):
     buf = ""
     for line in str(req):
         buf = buf + line
-    image_path = detector.download_and_resize_image(buf, 1280, 856, False)
+    image_path = detector.download_and_resize_image(buf, 1280, 856)
     return detector.run_detector(image_path)
